6586bec8603fb591506a4fb15fa00bdf4e3ef0e4
theEthicalDev <djordje.karadzic@cybethics.com>
2025-04-01 21:44:12 +0200

    Troubleshooting GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "",
+  base: "/cybethics-bridge/",
   plugins: [
     react(),
     mode === 'development' &&
