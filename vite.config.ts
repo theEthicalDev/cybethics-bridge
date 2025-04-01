3feacd53aefae66802d70d15eca68cd361c6bc43
theEthicalDev <djordje.karadzic@cybethics.com>
2025-04-01 23:33:35 +0200

    Troubleshooting GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return ({
+    base: './', // Add this line to set the base path
     server: {
       host: "::",
       port: 8080,
